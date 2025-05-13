5d39a9845f7a8ceb0fa170a13261bbcfc85db285
Hardik Chauhan <hardik11.chauhan@gmail.com>
2025-05-13 19:14:03 -0400

    fix: 🐛 fixed package.json script for commit

--- lo8ding-lib/src/lib/SimpleLoader/index.tsx
+++ lo8ding-lib/src/lib/SimpleLoader/index.tsx
@@ -10,7 +10,7 @@ export const SimpleLoader = ({
       className={`animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white dark:border-white ${className}`}
       style={style}
     >
-      Dem3
+      Demo
     </div>
   );
 };
